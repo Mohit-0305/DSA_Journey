ae2ace566891c981aa0da9b782e257a9bf11bb65
Mohit-0305 <2023bit017@sggs.ac.in>
2026-01-11 17:56:37 +0530

    Pattern 1 done

---
+++ pattern/pat1.cpp
@@ -0,0 +1,27 @@
+/*
+*********
+*********
+*********
+*********
+*********
+*********
+*********
+*********
+*********
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int input = 0;
+    cin>>input;
+    int i,j;
+    for(i=0;i<input;i++){
+        for(j=0;j<input;j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
