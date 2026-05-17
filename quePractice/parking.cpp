70841af9cf81a4d038e729d3367fb944103cf512
Mohit-0305 <2023bit017@sggs.ac.in>
2026-05-17 18:23:56 +0530

    practice questions

---
+++ quePractice/parking.cpp
@@ -0,0 +1,13 @@
+/*
+Problem: Most Filled Row in a Parking Lot
+Scenario: A parking lot has an RxC matrix layout. 
+Empty spots are 0 and filled spots are 1. 
+Find the index of the row that has the maximum number of 1s.
+
+Input:
+3 3
+0 1 0
+1 1 0
+1 1 1
+*/
+
