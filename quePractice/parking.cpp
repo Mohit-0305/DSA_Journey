7c0df3612ed7ee43eac7bf12d52b1038cc166f9b
Mohit-0305 <2023bit017@sggs.ac.in>
2026-05-18 15:15:50 +0530

    parking question

--- quePractice/parking.cpp
+++ quePractice/parking.cpp
@@ -11,3 +11,30 @@ Input:
 1 1 1
 */
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int R=0, C=0;
+    int total=0, max=0, row=-1;
+    cin>>R>>C;
+    int mat[R][C];
+
+    for(int i=0; i<R ; i++){
+        for(int j=0 ; j<C ; j++){
+            cin>>mat[i][j];
+        }
+    }
+
+    for(int i=0; i<R ; i++){
+        total=0;
+        for(int j=0 ; j<C ; j++){
+            total += mat[i][j]; 
+        }
+        if(total>max) max=total, row=i;
+    }
+
+    cout<<row;
+
+    return 0;
+}
