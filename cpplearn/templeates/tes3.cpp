7cbe853a5230a11d37f4512b5bd561431f3ed7b3
Mohit-0305 <2023bit017@sggs.ac.in>
2026-05-21 21:23:03 +0530

    added

---
+++ cpplearn/templeates/tes3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 10;
+    cout<<"Hello"<<endl;
+    // auto a = &x;
+    cout<< &x<<endl;
+    cout<<sizeof(x)<<endl;
+    // cout<<sizeof(a)<<endl;
+    return 0;
+}
