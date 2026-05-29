fabfcd7a67828270856412fa7cab90e6d9b8fb2a
Mohit-0305 <2023bit017@sggs.ac.in>
2026-05-29 15:04:01 +0530

    majority element logic

---
+++ quePractice/majority_element.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int candidate =0;
+        int count=0;
+
+        for(int n: nums){
+            if(count==0){
+                candidate=n;
+            }
+            if(n==candidate){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+};
